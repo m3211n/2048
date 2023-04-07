7c0a1a370ff6d89f5b0be8ff4236786e04322954
m3211n <130044007+m3211n@users.noreply.github.com>
2023-04-08 00:37:29 +0200

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,7 +3,193 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-
+            case "image1":
+            case "myImage":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 c c c 1 1 1 1 1 1 1 1 1 1 
+1 1 1 c 1 1 1 1 1 1 1 1 1 1 
+1 1 c c 1 1 1 1 1 1 1 1 1 1 
+1 c 1 1 1 1 1 1 1 1 1 1 1 1 
+1 c c c 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`;
+            case "image2":
+            case "myImage0":return img`
+d d d d d d d d d d d d d d 
+d c d c d d d d d d d d d d 
+d c d c d d d d d d d d d d 
+d c c c d d d d d d d d d d 
+d d d c d d d d d d d d d d 
+d d d c d d d d d d d d d d 
+d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d 
+`;
+            case "image11":
+            case "myImage9":return img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 1 1 1 2 1 1 1 2 1 1 1 2 2 
+2 1 2 1 2 1 2 1 2 1 2 1 2 2 
+2 1 1 1 2 1 2 1 2 1 2 1 2 2 
+2 1 2 1 2 1 2 1 2 1 2 1 2 2 
+2 1 1 1 2 1 1 1 2 1 1 1 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`;
+            case "image10":
+            case "myImage8":return img`
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 1 4 1 4 1 1 1 4 1 1 1 4 4 
+4 1 4 1 4 1 4 1 4 1 4 1 4 4 
+4 1 1 1 4 1 4 1 4 1 4 1 4 4 
+4 4 4 1 4 1 4 1 4 1 4 1 4 4 
+4 4 4 1 4 1 1 1 4 1 1 1 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+`;
+            case "image9":
+            case "myImage7":return img`
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 e e e 5 e e e 5 e e e 5 5 
+5 5 5 e 5 e 5 e 5 e 5 e 5 5 
+5 e e e 5 e 5 e 5 e 5 e 5 5 
+5 e 5 5 5 e 5 e 5 e 5 e 5 5 
+5 e e e 5 e e e 5 e e e 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+`;
+            case "image8":
+            case "myImage6":return img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 8 7 8 8 8 7 8 8 8 7 7 7 7 
+7 8 7 8 7 8 7 8 7 8 7 7 7 7 
+7 8 7 8 7 8 7 8 7 8 7 7 7 7 
+7 8 7 8 7 8 7 8 7 8 7 7 7 7 
+7 8 7 8 8 8 7 8 8 8 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`;
+            case "image7":
+            case "myImage5":return img`
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 8 8 8 9 8 8 8 9 9 9 9 9 9 
+9 8 9 8 9 8 9 8 9 9 9 9 9 9 
+9 8 8 8 9 8 9 8 9 9 9 9 9 9 
+9 8 9 8 9 8 9 8 9 9 9 9 9 9 
+9 8 8 8 9 8 8 8 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+`;
+            case "image6":
+            case "myImage4":return img`
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 1 8 1 8 1 1 1 8 8 8 8 8 8 
+8 1 8 1 8 1 8 1 8 8 8 8 8 8 
+8 1 1 1 8 1 8 1 8 8 8 8 8 8 
+8 8 8 1 8 1 8 1 8 8 8 8 8 8 
+8 8 8 1 8 1 1 1 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+`;
+            case "image5":
+            case "myImage3":return img`
+a a a a a a a a a a a a a a 
+a 1 1 1 a 1 1 1 a a a a a a 
+a a a 1 a 1 a 1 a a a a a a 
+a a 1 1 a 1 a 1 a a a a a a 
+a 1 a a a 1 a 1 a a a a a a 
+a 1 1 1 a 1 1 1 a a a a a a 
+a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a 
+`;
+            case "image4":
+            case "myImage2":return img`
+c c c c c c c c c c c c c c 
+c 1 c 1 1 1 c c c c c c c c 
+c 1 c 1 c 1 c c c c c c c c 
+c 1 c 1 c 1 c c c c c c c c 
+c 1 c 1 c 1 c c c c c c c c 
+c 1 c 1 1 1 c c c c c c c c 
+c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c 
+`;
+            case "image3":
+            case "myImage1":return img`
+b b b b b b b b b b b b b b 
+b 1 1 1 b b b b b b b b b b 
+b 1 b 1 b b b b b b b b b b 
+b 1 1 1 b b b b b b b b b b 
+b 1 b 1 b b b b b b b b b b 
+b 1 1 1 b b b b b b b b b b 
+b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b 
+`;
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
-
+namespace SpriteKind {
+    export const Number = SpriteKind.create()
+}
+function initTiles () {
+    list = [
+    sprites.create(assets.image`myImage`, SpriteKind.Number),
+    sprites.create(assets.image`myImage0`, SpriteKind.Number),
+    sprites.create(assets.image`myImage1`, SpriteKind.Number),
+    sprites.create(assets.image`myImage2`, SpriteKind.Number),
+    sprites.create(assets.image`myImage3`, SpriteKind.Number),
+    sprites.create(assets.image`myImage4`, SpriteKind.Number),
+    sprites.create(assets.image`myImage5`, SpriteKind.Number),
+    sprites.create(assets.image`myImage6`, SpriteKind.Number),
+    sprites.create(assets.image`myImage7`, SpriteKind.Number),
+    sprites.create(assets.image`myImage8`, SpriteKind.Number),
+    sprites.create(assets.image`myImage9`, SpriteKind.Number)
+    ]
+    return list
+}
+let list: Sprite[] = []
+let numbersTiles = initTiles()
+scene.setBackgroundColor(3)
+tiles.setCurrentTilemap(tilemap`level1`)
